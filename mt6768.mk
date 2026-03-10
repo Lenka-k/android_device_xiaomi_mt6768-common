421b08acddfb9dc323401a587438c0898d56b6bb
Lenka-k <84929866+Lenka-k@users.noreply.github.com>
2026-03-10 17:07:46 +0530

    removing some un-necessary packages

--- mt6768.mk
+++ mt6768.mk
@@ -231,8 +231,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
     $(LOCAL_PATH)/configs/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
-PRODUCT_PACKAGES += \
-    libui-v32
+#PRODUCT_PACKAGES += \
+#    libui-v32
 
 # USB
 PRODUCT_PACKAGES += \
